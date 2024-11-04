74a8caa663b1a8bff1d18f0b4a1c13b9e28a63d7
reyharighy <arighymoch@gmail.com>
2024-11-04 15:34:31 +0700

    First codebase
    Signup dan Signin

---
+++ signup.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+</head>
+
+<!-- Body Section -->
+<body>
+
+</body>
+<!-- End of Body Section -->
+
+<!-- Footer Section -->
+<footer>
+
+</footer>
+</html>
